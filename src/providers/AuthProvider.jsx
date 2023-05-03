fa6e7a650c6822904736995dad13c62ae8a57734
Md. Yasin Arafat <mdyasinarafathasib@gmail.com>
2023-05-04 02:37:34 +0600

    Fixing reload problem of the private route done

--- src/providers/AuthProvider.jsx
+++ src/providers/AuthProvider.jsx
@@ -13,16 +13,20 @@ export const AuthContext = createContext(null);
 const auth = getAuth(app);
 const AuthProvider = ({ children }) => {
   const [user, setUser] = useState(null);
+  const [loading, setLoading] = useState(true);
 
   const createUser = (email, password) => {
+    setLoading(true);
     return createUserWithEmailAndPassword(auth, email, password);
   };
 
   const logIn = (email, password) => {
+    setLoading(true);
     return signInWithEmailAndPassword(auth, email, password);
   };
 
   const logOut = () => {
+    setLoading(true);
     return signOut(auth);
   };
 
@@ -30,6 +34,7 @@ const AuthProvider = ({ children }) => {
     const unsubscribe = onAuthStateChanged(auth, (loggedUser) => {
       console.log(loggedUser);
       setUser(loggedUser);
+      setLoading(false);
     });
     return () => {
       unsubscribe();
@@ -41,6 +46,7 @@ const AuthProvider = ({ children }) => {
     createUser,
     logIn,
     logOut,
+    loading,
   };
 
   return (
--- src/routes/PrivateRoute.jsx
+++ src/routes/PrivateRoute.jsx
@@ -1,10 +1,18 @@
 import React, { useContext } from "react";
 import { AuthContext } from "../providers/AuthProvider";
 import { Navigate, useLocation } from "react-router-dom";
+import { Spinner } from "react-bootstrap";
 
 const PrivateRoute = ({ children }) => {
-  const { user } = useContext(AuthContext);
+  const { user, loading } = useContext(AuthContext);
   const location = useLocation();
+  if (loading) {
+    return (
+      <div className="d-flex justify-content-center align-items-center mt-5">
+        <Spinner className="fs-5 mt-5" animation="border" variant="danger" />
+      </div>
+    );
+  }
 
   if (user) {
     return children;
