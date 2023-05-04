92cba66417c13f8f2d4e6591f0c57203780d689c
Md. Yasin Arafat <mdyasinarafathasib@gmail.com>
2023-05-04 19:52:36 +0600

    Showing a spinner when the data is in a loading state has done

--- src/layouts/Main.jsx
+++ src/layouts/Main.jsx
@@ -1,8 +1,23 @@
 import Footer from "../pages/shared/Footer/Footer";
 import { Outlet } from "react-router-dom";
 import NavigationBar from "../pages/shared/Navigationbar/Navigationbar";
+import { useContext } from "react";
+import { AuthContext } from "../providers/AuthProvider";
+import { Spinner } from "react-bootstrap";
 
 const Main = () => {
+  // loading spinner here
+  const { loading } = useContext(AuthContext);
+  if (loading) {
+    return (
+      <div
+        style={{ marginTop: "300px" }}
+        className="d-flex justify-content-center align-items-center"
+      >
+        <Spinner className="fs-6" animation="border" variant="danger" />
+      </div>
+    );
+  }
   return (
     <div>
       <NavigationBar></NavigationBar>
